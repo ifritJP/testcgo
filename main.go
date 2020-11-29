3eb17e3a3d5b580a96aa480876ff199c19560191
ifritJP <15117169+ifritJP@users.noreply.github.com>
2020-11-29 09:31:03 +0900

    add cgo.

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package testcgo
 
+// #include <string.h>
+// #include <stdlib.h>
+// #cgo CFLAGS: -I/usr/include/lua5.3
+// #cgo LDFLAGS: -ldl -lm -llua5.3
+// #include <lauxlib.h>
+// #include <lualib.h>
+import "C"
+
 import "fmt";
 
 func Sub() {
     fmt.Println( "hogea:5" )
 }
+
+type lua_int = C.longlong
+type lua_num = C.double
+type lua_bool = C.int
+
+func luaL_newstate() *C.lua_State {
+    return C.luaL_newstate()
+}
+func lua_close(vm *C.lua_State) {
+    C.lua_close( vm )
+}
+func lua_version(vm *C.lua_State) lua_num {
+    return *C.lua_version( vm )
+}
+
